979352754ab6fd0afabf03ce69f39da50034d633
HeroRickyGAMES <ricojn9@gmail.com>
2024-08-07 15:43:53 -0300

    Web ads foi adicionado!

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3367803620"
+    serviceWorkerVersion: "2954356347"
   }
 });
